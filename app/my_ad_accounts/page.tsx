55cb5494bd35ee8bf5548290e8ee37d0d669228f
Sandeep Bhat <sbhat2@meta.com>
2026-03-12 22:13:57 -0700

    Ad account card update accoording to figma
    Summary:
    * Update my_ad_accounts page layout per figma
       * Add side bar to my waba page
       * Update listing of wabas
    *  [Figma](https://www.figma.com/make/ccTsicCvCp8kxeHLn8r244/Sample-App--KXL-Consulting--for-TP?p=f&t=P43SEbPhw6C0Sy2o-0&fullscreen=1)
    
    Reviewed By: sbchengyiwei111
    
    Differential Revision: D96403527
    
    fbshipit-source-id: 5b0f5d25befb4e87b516c19af890a90adf8030bc

--- app/my_ad_accounts/page.tsx
+++ app/my_ad_accounts/page.tsx
@@ -3,12 +3,12 @@
 // This source code is licensed under the MIT license found in the
 // LICENSE file in the root directory of this source tree.
 
-
-import LoggedOut from "@/app/components/LoggedOut";
-import { getAdAccounts } from '@/app/api/be_utils';
 import { auth0 } from "@/lib/auth0";
-import AssetPage from '@/app/components/AssetPage';
-import AssetCard from '@/app/components/AssetCard';
+import { getAdAccounts, getAppDetails } from "@/app/api/be_utils";
+import LoggedOut from "@/app/components/LoggedOut";
+import WabaPageLayout from "@/app/components/WabaPageLayout";
+import AdAccountCard from "@/app/components/AdAccountCard";
+import publicConfig from "@/app/public_config";
 
 interface AdAccount {
   ad_account_id: string;
@@ -18,39 +18,38 @@ interface AdAccount {
 }
 
 export default async function MyAdAccounts() {
-  // Fetch the user session
   const session = await auth0.getSession();
 
-  // If no session, show the logged out component
   if (!session) {
     return <LoggedOut />;
   }
 
-  const user_id = session.user.email;
+  const userId = session.user.email;
+  const appId = publicConfig.app_id;
+  const appDetails = await getAppDetails(appId);
+  const app_name = appDetails.name;
+  const logo_url = appDetails.logo_url;
 
-  // Fetch ad account names from Facebook Graph API
-  const adAccountsWithNames = await getAdAccounts(user_id);
+  const adAccounts = await getAdAccounts(userId);
 
   return (
-    <AssetPage
+    <WabaPageLayout
       title="My Ad Accounts"
-      user_id={user_id}
-      isEmpty={adAccountsWithNames.length === 0}
+      user_id={userId}
+      logo_url={logo_url}
+      app_name={app_name}
+      isEmpty={adAccounts.length === 0}
       emptyMessage="No ad accounts found. Ad accounts will appear here once they are connected to your account."
     >
-      {adAccountsWithNames.map((account: AdAccount) => (
-        <AssetCard
+      {adAccounts.map((account: AdAccount) => (
+        <AdAccountCard
           key={account.ad_account_id}
-          id={account.ad_account_id}
-          title={account.name || 'Unnamed Account'}
-          access_token={account.access_token || ''}
-          business_id={account.business_id}
-          selected_asset_type="ad-account"
-          path="ad_accounts"
-          action_url={`https://adsmanager.facebook.com/adsmanager/manage/accounts?act=${account.ad_account_id}`}
-          action_title="View in Ads Manager"
+          ad_account_id={account.ad_account_id}
+          name={account.name || "Unnamed Account"}
+          access_token={account.access_token || ""}
+          business_id={account.business_id || ""}
         />
       ))}
-    </AssetPage>
+    </WabaPageLayout>
   );
 }
